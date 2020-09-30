bf674db92119fa0ac58a74f776c6180d658f5917
sarvagya230 <68740017+sarvagya230@users.noreply.github.com>
2020-09-30 16:46:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -32,26 +32,42 @@ function draw()
   var end =map(sec,0,60,0,360)
   arc(0,0,300,300,-90,end-90)
   push()
-  rotate(0)
+  rotate(end)
   strokeWeight(4)
-  stroke(255)
+  stroke(255,100,150)
   
-  line(0,0,0,end)
+  line(0,-100,0,0)
   
   pop()
 
   stroke(150,100,150)
   var end2 =map(min,0,60,0,360)
   arc(0,0,280,280,-90,end2-90)
+  push()
+  rotate(end2)
+  strokeWeight(4)
+  stroke(150,100,150)
+  
+  line(0,-70,0,0)
+  
+  pop()
 
   stroke(150,255,100)
   var end3 =map(hr%12,0,12,0,360)
   arc(0,0,260,260,-90,end3-90)
+  push()
+  rotate(end3)
+  strokeWeight(4)
+  stroke(150,255,100)
+  
+  line(0,-50,0,0)
+  
+  pop()
   pop()
   push()
   fill(255)
 
-  text(hr+':'+min+':'+sec,10,10)
+  text(hr+':'+min+':'+sec,-150,-150)
   pop()
 
 
